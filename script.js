1f72b6fc0846306b0efdf95a5dc3e620e9ec1983
DanielRodrigoBeltrame <93795810+DanielRodrigoBeltrame@users.noreply.github.com>
2021-11-11 22:08:03 -0300

    Atualização 11.11
    Iniciando aplicação contador

---
+++ script.js
@@ -0,0 +1,23 @@
+const data=document.getElementById("data")
+
+function somar() {
+let number=parseInt(data.innerText)
+number++
+data.innerText=number
+}
+function diminuir(){
+    let number=parseInt(data.innerText)
+    number--
+    data.innerText=number
+}
+function reset(){
+    let number=parseInt(data.innerText)
+    number=0
+    data.innerText=number
+}
+function incluir(){
+    let number=parseInt(data.innerText)
+
+    lista1.innerText=number
+}
+
